fb4470787947661beb14fd679ac7676a644143c9
hanchong <13691268076@163.com>
2019-11-22 16:53:20 +0800

    入参加@ResquestBody

--- bootDemo/src/main/java/com/example/bootDemo/tableBook/controller/ExpenseCalendarController.java
+++ bootDemo/src/main/java/com/example/bootDemo/tableBook/controller/ExpenseCalendarController.java
@@ -7,7 +7,9 @@ import com.example.bootDemo.tableBook.service.ExpenseCalendarService;
 import com.example.bootDemo.tableBook.vo.ExpenseCalendarVO;
 import com.example.bootDemo.tableBook.vo.ExpenseSearchVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -27,7 +29,7 @@ public class ExpenseCalendarController {
 
     //主列表查询
     @RequestMapping("/list")
-    public ResponseVO list(ExpenseSearchVO searchVO) {
+    public ResponseVO list(@RequestBody ExpenseSearchVO searchVO) {
 
         try {
             PageResponseVO<ExpenseCalendarVO> responseVO = calendarService.list(searchVO);
